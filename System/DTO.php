63885b4f3c1d313adaa23ec1d6a04959a9403b43
Dmitry Safronov <aa502@yandex.ru>
2022-06-01 00:15:24 +0700

    feat: Базовый DTO

---
+++ System/DTO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace System;
+
+class DTO
+{
+    public function __construct(array $data)
+    {
+        foreach ($data as $key => $item){
+            if(property_exists($this, $key)){
+                $this->{$key} = $item;
+            }
+        }
+    }
+}
