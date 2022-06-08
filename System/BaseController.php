cc36c6ec0187de5aedd0fbc6cf8d36b302ba6f03
Dmitry Safronov <aa502@yandex.ru>
2022-06-08 23:22:37 +0700

    feat: Прием заголовков

--- System/BaseController.php
+++ System/BaseController.php
@@ -8,10 +8,10 @@ class BaseController implements IController
 {
     protected array $env = [];
 
-    public function setEnv(array $urlParams, array $get, array $post, array $server) : void{
+    public function setEnv(array $urlParams, array $get, array $post, array $headers) : void{
         $this->env['params'] = $urlParams;
         $this->env['get'] = $get;
         $this->env['post'] = $post;
-        $this->env['server'] = $server;
+        $this->env['headers'] = $headers;
     }
 }
